97de02f99e7811f41476fd2596328481aca418e6
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-30 22:32:15 +0500

    some fix

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function Layot({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Header />
             {children}
+            <Footer />
         </>
     )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full py-2 bg-blue-500">
+            footer
+        </footer>
+    )
+}
