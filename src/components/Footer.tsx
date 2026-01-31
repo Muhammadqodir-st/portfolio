124d7690e0abf9aee4a484108ece32c8ba035231
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-31 14:20:01 +0500

    header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
+// lucide
+import { Github, Mail, Send } from "lucide-react";
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer className="w-full py-2 bg-blue-500">
-            footer
+        <footer className="w-full py-2">
+            <div className="max-w-249.5 w-[90%] m-auto flex items-center justify-between">
+                <p className="text-gray-600">© 2026 /Muhammadqodir Ma'murjonov</p>
+
+                <div className="flex items-center gap-4">
+                    <Link href={'https://github.com/Muhammadqodir-st'} className="py-1 px-1 cursor-pointer rounded-lg text-gray-600" title="GitHub"><Github size={23} /></Link>
+                    <Link href={'https://t.me/mukhamadqodir'} className="py-1 px-1 cursor-pointer rounded-lg text-gray-600" title="Telegram"><Send size={23} /></Link>
+                    <Link href={'mamurjonov.muhammadqodir01@gmail.com'} className="py-1 px-1 cursor-pointer rounded-lg text-gray-600" title="Mail"><Mail size={23} /></Link>
+                </div>
+            </div>
         </footer>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
+// next
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <header className="w-full py-2 bg-red-500">
-            header
+        <header className="w-full bg-gray-200 py-4">
+            <div className="max-w-249.5 w-[90%] m-auto flex items-center justify-between">
+                <p className="text-xl font-bold">Muhammadqodir</p>
+
+                <ul className="flex items-center gap-8">
+                    <li><Link className="text-gray-600 hover:text-gray-500 transition delay-75" href={'/'}>Home</Link></li>
+                    <li><Link className="text-gray-600 hover:text-gray-500 transition delay-75" href={'/'}>About</Link></li>
+                    <li><Link className="text-gray-600 hover:text-gray-500 transition delay-75" href={'/'}>Work</Link></li>
+                    <li><Link className="text-gray-600 hover:text-gray-500 transition delay-75" href={'/'}>Contact</Link></li>
+                </ul>
+            </div>
         </header>
     )
 }
