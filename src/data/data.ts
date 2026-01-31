6d8a977b8798d6999ed40da1107b6c5509958037
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-31 14:40:30 +0500

    some fix

---
+++ src/data/data.ts
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        name: 'Shopstore',
+        image: '/assets/project-1.png',
+        description: 'This project ',
+        link: 'https://shopstore-com.vercel.app/',
+        gitHubLink: 'https://github.com/Muhammadqodir-st/shopstore__client'
+    },
+    {
+        name: 'Dropix',
+        image: '/assets/project-2.png',
+        description: 'This project ',
+        link: 'https://dropixuz.vercel.app/',
+        gitHubLink: 'https://github.com/Muhammadqodir-st/dropix_client'
+    },
+]
